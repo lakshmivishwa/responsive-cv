281bada8874ae5e23efc5efa2265632646d92a9d
lakshmivishwa <lakshmi.vishwakarma@excellarate.com>
2023-07-27 15:54:40 +0530

    first commit

---
+++ resume/src/App.js
@@ -0,0 +1,16 @@
+
+import Homepage from './components/Homepage/Homepage';
+import PersonalDetail from './components/PersonalDetail/PersonalDetail';
+import Careers from './components/Careers/careers';
+function App() {
+  return (
+    <div className="App">
+      <Homepage />
+      <PersonalDetail />
+      <Careers />
+
+    </div>
+  );
+}
+
+export default App;
