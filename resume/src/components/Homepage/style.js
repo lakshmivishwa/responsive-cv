da7341213ee9570c11017ad8bb5488c12c98b7d2
lakshmivishwa <lakshmi.vishwakarma@excellarate.com>
2023-08-23 18:33:16 +0530

    added images

--- resume/src/components/Homepage/style.js
+++ resume/src/components/Homepage/style.js
@@ -1,14 +1,10 @@
 const styles = {
-
-
     Container: {
-        backgroundImage: "url(http://localhost:3000/image/photo.jpg)",
-        backgroundSize: "100% 100%",
-        // opacity: 0.5,
         color: "black ",
-        // backgroundColor: "#fabd69",
         padding: " 400px 0",
         textAlign: "center",
+        backgroundImage: "url(http://localhost:3000/image/homepage.jpg)",
+        backgroundSize: "100% 100%",
     },
 
 
--- resume/src/components/PersonalDetail/style.js
+++ resume/src/components/PersonalDetail/style.js
@@ -1,10 +1,6 @@
 const styles = {
-
-
     imageContainer: {
         border: "1px solid black",
-        // marginTop: "30px",
-        // textAlign:"center",
         width: "80%",
         height: "250px",
         borderRadius: "100px"
