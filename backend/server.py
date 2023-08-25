c3c4891e610fc460576b410ed99e798b7fb0e0f4
lakshmivishwa <lakshmi.vishwakarma@excellarate.com>
2023-08-25 16:28:10 +0530

    Database connected

--- backend/server.py
+++ backend/server.py
@@ -1,9 +1,25 @@
-from flask import Flask, jsonify, request,send_file
+# from flask import Flask, jsonify, request,send_file
+# from pymongo import MongoClient
+# app = Flask(__name__)
+# @app.route('/')
+# def hello():
+#     return "hello"
+
+from flask import Flask,jsonify
+from pymongo import MongoClient
 
 app = Flask(__name__)
 
-@app.route('/my-first-api')
-def hello():
-    return "hello"
+app.config['MONGO_URI'] = 'mongodb://localhost:27017/resume'
+mongo = MongoClient(app.config['MONGO_URI'])
+db = mongo.resume
 
-  
+collection = db.personal_detail
+
+@app.route('/get_data_json', methods=['GET'])
+def get_data_json():
+    data = list(collection.find({}))
+    return jsonify(data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
